0e8781197f978b9130e70af023e14627c2c1471f
usama2333 <usama.wizz@gmail.com>
2023-06-17 10:47:29 +0500

    added empty componnets and routing

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ball = () => {
+  return (
+    <div>
+      Ball
+    </div>
+  )
+}
+
+export default Ball
---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Earth = () => {
+  return (
+    <div>
+      Earth
+    </div>
+  )
+}
+
+export default Earth
