62ada8d236c92634ea3e737934a36798d2bb9b15
usama2333 <usama.wizz@gmail.com>
2023-06-26 09:49:35 +0500

    working on experience section

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ const App = () => {
           <Hero />
         </div>
       <About/>
-          {/* <Experience />
-        <Tech />
+          <Experience />
+         {/*<Tech />
         <Works />
         <Feedbacks /> */}
         <div className='relative z-0'>
