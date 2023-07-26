a8d505af835eb6bb1509499061d47861e8f782e0
usama2333 <usama.wizz@gmail.com>
2023-07-26 21:46:33 +0500

    added starts in the background of contact section

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         <div className='relative z-0'>
           <Contact />
           {/* <StarsCanvas /> */}
-          {/* <Stars/> */}
+          <Stars/>
         </div>
       </div>
     </BrowserRouter>
