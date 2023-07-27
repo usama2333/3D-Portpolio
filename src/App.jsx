5b1bd74dc8c827baf68708100116b946d0723187
usama2333 <usama.wizz@gmail.com>
2023-07-27 10:35:22 +0500

    hide project section

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
       <About/>
           <Experience />
         <Tech />
-        <Works />
+        {/* <Works /> */}
          {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
