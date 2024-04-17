1aa7455c8779ad99f2904da2b213ed155cd74774
usama2333 <usama.wizz@gmail.com>
2024-04-17 16:18:52 +0500

    remove extra images

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,9 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
@@ -21,27 +19,18 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import countryProject from "./countryProject.png";
 import dashboardProject from "./dashboardProject.png";
 import liboexProject from "./liboexProject.png";
 import nynxProject from "./nynxProject.png";
-import socialmediaProject from "./socialmediaProject.png";
+import socialmediaProject from "./socialmediaProject.jpg";
 import task1 from "./task1.png";
 import todoList from "./todoList.png";
-import solarProject from "./solarProject.png";
+import solarProject from "./solarProject.jpg";
 import v2fExchangeProject from "./v2fExchangeProject.png";
 import codeIt from "./codeIt.png";
-import agent1 from "./agent1.png";
-import agent2 from "./agent2.png";
+import agent2 from "./agent2.jpg";
 import dashboard from "./dashboard.png";
-import popcard from "./popcard.png";
-import popcard1 from "./popcard1.png";
 import opticlac from "./opticlac.png";
 
 export {
@@ -54,7 +43,6 @@ export {
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
@@ -67,12 +55,6 @@ export {
   typescript,
   threejs,
   meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   countryProject,
   dashboardProject,
   liboexProject,
@@ -83,10 +65,7 @@ export {
   todoList,
   v2fExchangeProject,
   codeIt,
-  agent1,
   agent2,
   dashboard,
-  popcard,
-  popcard1,
   opticlac
 };
