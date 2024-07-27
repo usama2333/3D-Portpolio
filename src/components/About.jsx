4e4edc22481749db1f9bbed61868e3546435ce04
usama2333 <usama.wizz@gmail.com>
2024-07-27 14:14:25 +0500

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-
+// hello
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
